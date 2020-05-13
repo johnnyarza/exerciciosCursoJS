ce070f0f7d50badd47ae59522ce536df2673a8ab
johnnyarza <johnnyarza@gmail.com>
2020-05-13 15:12:07 -0300

    ex3

---
+++ 03.js
@@ -0,0 +1,7 @@
+//03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function expoente(base, exp) {
+    return base ** exp
+}
+
+console.log(expoente(2,3))
