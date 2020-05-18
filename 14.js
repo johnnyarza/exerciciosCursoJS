409cd150d397bcac00437e9a6b676a37f69c7597
johnnyarza <johnnyarza@gmail.com>
2020-05-18 14:06:03 -0300

    ex14

---
+++ 14.js
@@ -0,0 +1,36 @@
+/**14) Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três
+casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com
+escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie
+também um default, que retornará uma mensagem de erro no console. */
+
+function verificaFruta (fruta = "") {
+        if (typeof(fruta) != "string" || 
+            (
+                fruta.toLowerCase() != "kiwi" && 
+                fruta.toLowerCase() != "melancia" && 
+                fruta.toLowerCase() != "maçã"
+            )) {
+            throw error = {message: `Fruta (${fruta}) inválida`}
+        }
+}
+
+function vendeFruta(fruta = "") {
+    try {
+        verificaFruta(fruta)
+        switch(fruta.toLowerCase()) {
+            case 'maçã':
+                console.log("Não vendemos esta fruta aqui")
+                break
+            case 'kiwi':
+                console.log("Estamos com escassez de kiwis")
+                break
+            case 'melancia':
+                console.log('Aqui está, são 3 reais o quilo')
+                break
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+vendeFruta('kiwi')
