1bc6b35193aab426552c6e2b681350533c7d1eaa
johnnyarza <johnnyarza@gmail.com>
2020-05-20 14:43:43 -0300

    ex25

---
+++ 25.js
@@ -0,0 +1,9 @@
+//25) Escrever um programa para exibir os números de 1 até 50 na tela.
+
+function printNumber(max) {
+  for (let i = 1; i <= max; i++) {
+    console.log(i);
+  }
+}
+
+printNumber(50);
